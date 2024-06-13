bcd4fa107a818bf8e873dd7a2e6a3e16879d6d0b
KimRiun <kimriun27@gmail.com>
2024-06-13 09:31:05 +0900

    chore : tailwind config 설정

--- src/routes/main/page.jsx
+++ src/routes/main/page.jsx
@@ -1,7 +1,20 @@
-export default function MainPage(){
-    return (
-        <div>
-            Main
-        </div>
-    )
+export default function MainPage() {
+  return (
+    <>
+      <div className=" bg-tuatara-950">
+        <div className="text-hero text-tuatara-50">Hero</div>
+        <div className="text-heading1 text-tuatara-50">제목</div>
+        <div className="text-heading2 text-tuatara-50">제목</div>
+        <div className="text-heading3 text-tuatara-50">제목</div>
+        <div className="text-subheading text-tuatara-50">부제목</div>
+        <div className="text-body1 text-tuatara-50">내용</div>
+        <div className="text-body2 text-tuatara-50">내용</div>
+        <div className="text-caption text-tuatara-50">캡션</div>
+        <div className="text-mini text-tuatara-50">미니</div>
+      </div>
+      <div className=" text-heading3 bg-blue-400 font-abril">Font</div>
+      <div className=" text-body1 bg-blue-400 font-apple font-bold">폰트</div>
+      <div className=" text-body1 bg-blue-400 font-apple">폰트</div>
+    </>
+  );
 }
