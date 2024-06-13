cc3fbdc7f170d06a33f48a65b75e4595d23a8ec5
KimRiun <kimriun27@gmail.com>
2024-06-14 08:51:41 +0900

    feat : 템플릿 생성 화면에 종목, 템플릿, 목표주가, 상승여력 표시

--- src/routes/valuationCreateExcel/page.jsx
+++ src/routes/valuationCreateExcel/page.jsx
@@ -6,5 +6,23 @@ export default function ValuationCreateExcel() {
 
   console.log(searchParams.get("id"), searchParams.get("template"));
 
-  return <div>ValuationCreateExcel</div>;
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+      <div className="p-2 text-heading2">목표 주가 계산표</div>
+      <div className="flex gap-2">
+        <div className="flex">{`종목:  ${searchParams.get("id")}`}</div>
+        <p className="flex">{`템플릿:  ${searchParams.get("template")}`}</p>
+      </div>
+      <div className="flex gap-4">
+        <div className="flex gap-2">
+          목표주가
+          <input type="number" className="text-tuatara-100 bg-tuatara-900" />원
+        </div>
+        <div className="flex gap-2">
+          상승여력
+          <input type="number" className="text-tuatara-100 bg-tuatara-900" />%
+        </div>
+      </div>
+    </div>
+  );
 }
