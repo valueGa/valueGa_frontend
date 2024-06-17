e77e6f898e3254c12fa00663fa22220ad20fbfcd
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-06-17 16:48:13 +0900

    feat : Service탭 UI 추가

--- src/routes/main/TabService.jsx
+++ src/routes/main/TabService.jsx
@@ -1,3 +1,45 @@
 export default function Service() {
-  return <div>Service</div>;
+  return (
+    <div className="bg-tuatara-900 min-h-screen">
+      <div className="flex items-center justify-center pt-10">
+        <p className="mt-10 text-subheading text-tuatara-50 font-abril">
+          Service
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center pt-20">
+        <div className="pl-7 pr-7">
+          <div className="flex items-center justify-center gap-5 mb-3">
+            <img className="w-8 h-8" src="/public/ic_expression.svg" alt="" />
+            <p className="text-subheading text-tuatara-50 font-serif">
+              Valuation
+            </p>
+          </div>
+          <div className="pl-20 pr-20 pt-10 pb-10 bg-gradient-to-b from-[#2D2D2D] to-tuatara-900">
+            <p className="text-caption text-tuatara-50">
+              나만의 밸류에이션 템플릿 생성 및 관리
+              <br /> 자동으로 필요한 재무 데이터 불러오기
+            </p>
+          </div>
+        </div>
+
+        <div className="pl-7 pr-7">
+          <div className="flex items-center justify-center gap-5 mb-3">
+            <img className="w-8 h-8" src="/public/ic_chart.svg" alt="" />
+            <p className="text-subheading text-tuatara-50 font-serif">
+              Consensus
+            </p>
+          </div>
+          <div className="pl-20 pr-20 pt-10 pb-10 bg-gradient-to-b from-[#2D2D2D] to-tuatara-900">
+            <p className="text-caption text-tuatara-50">
+              다른 애널리스트들의 컨센서스 확인
+              <br /> 목표 주가 및 상승 여력 보기
+            </p>
+          </div>
+        </div>
+
+        <div></div>
+      </div>
+    </div>
+  );
 }
