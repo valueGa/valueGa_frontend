2d37cdb81e91f674eba833ef6dc7271b798ff4aa
ChaeyoonWoo <heeni1004@gmail.com>
2024-06-20 11:41:02 +0900

    feat: 저장, 임시저장 api 연결
    #22

--- src/components/consensus/valuation/ExcelFooter.jsx
+++ src/components/consensus/valuation/ExcelFooter.jsx
@@ -1,12 +1,16 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
-export default function ExcelFooter() {
+export default function ExcelFooter({ onSave, onTemporarySave }) {
   return (
     <div className="py-4 flex flex-col gap-2">
       <div className="flex justify-center gap-4">
-        <Button className=" bg-blue-500 border-none">임시저장</Button>
-        <Button className=" bg-blue-400 border-none">저장</Button>
+        <Button className=" bg-blue-500 border-none" onClick={onTemporarySave}>
+          임시저장
+        </Button>
+        <Button className=" bg-blue-400 border-none" onClick={onSave}>
+          저장
+        </Button>
       </div>
       <div className="text-tuatara-300">
         *임시 저장은 최대 3개까지 가능합니다.
