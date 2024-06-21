75db6ac2c00c1b1614858fe4e7dfa33d639f23dc
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-06-21 14:20:34 +0900

    refactor : 종가 차트 UI 수정

--- src/components/consensus/home/detail/ChartStock.jsx
+++ src/components/consensus/home/detail/ChartStock.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import ReactApexChart from "react-apexcharts";
+import { colors } from "/tailwind.config.js"; // 실제 경로로 수정하세요
 
 const ApexChart = () => {
   const [series] = useState([
     {
       name: "Desktops",
-      data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+      data: [
+        10, 31, 24, 50, 45, 47, 40, 52, 1, 32, 4, 52, 32, 21, 41, 35, 51, 49,
+        62, 69, 91, 148,
+      ],
     },
   ]);
 
   const [options] = useState({
+    // colors: [colors.pink[200]],
+    stroke: {
+      show: true,
+      lineCap: "straight",
+      colors: [colors.pink[200]],
+      width: 1,
+      dashArray: 0,
+    },
     chart: {
       height: 350,
+      background: "#3D3D3D",
       toolbar: {
         show: false,
       },
@@ -24,27 +37,46 @@ const ApexChart = () => {
     dataLabels: {
       enabled: false,
     },
-    stroke: {
-      curve: "straight",
-    },
+
     title: {
       show: false,
       align: "left",
     },
     grid: {
+      show: false,
+      borderColor: "#90A4AE",
+      position: "back",
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
       row: {
-        colors: ["transparent"], // takes an array which will be repeated on columns
+        colors: undefined,
         opacity: 0.5,
       },
       column: {
-        colors: ["transparent"], // takes an array which will be repeated on columns
+        colors: undefined,
         opacity: 0.5,
+      },
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
       },
     },
     yaxis: {
       show: false,
     },
     xaxis: {
+      type: "category",
+
       categories: [
         "Jan",
         "Feb",
@@ -56,6 +88,76 @@ const ApexChart = () => {
         "Aug",
         "Sep",
       ],
+      tickAmount: undefined,
+      tickPlacement: "between",
+      min: undefined,
+      max: undefined,
+      stepSize: undefined,
+      range: undefined,
+      floating: false,
+      decimalsInFloat: undefined,
+      overwriteCategories: undefined,
+      position: "bottom",
+      labels: {
+        show: true,
+        rotate: -45,
+        rotateAlways: false,
+        hideOverlappingLabels: true,
+        showDuplicates: false,
+        trim: false,
+        minHeight: undefined,
+        maxHeight: 120,
+        style: {
+          colors: [],
+          fontSize: "12px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 400,
+          cssClass: "apexcharts-xaxis-label",
+        },
+        offsetX: 0,
+        offsetY: 0,
+        format: undefined,
+        formatter: undefined,
+        datetimeUTC: true,
+        datetimeFormatter: {
+          year: "yyyy",
+          month: "MMM 'yy",
+          day: "dd MMM",
+          hour: "HH:mm",
+        },
+      },
+      group: {
+        groups: [],
+        style: {
+          colors: [],
+          fontSize: "12px",
+          fontWeight: 400,
+          fontFamily: undefined,
+          cssClass: "",
+        },
+      },
+      axisBorder: {
+        show: false,
+        color: "#78909C",
+        height: 1,
+        width: "100%",
+        offsetX: 0,
+        offsetY: 0,
+      },
+      axisTicks: {
+        show: false,
+        borderType: "solid",
+        color: "#78909C",
+        height: 6,
+        offsetX: 0,
+        offsetY: 0,
+      },
+      crosshairs: {
+        show: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
     },
   });
 
