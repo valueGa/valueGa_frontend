5fd78cae7f919df35aff0f63f2ad95fa895b370d
Ho-Da-Dak <godqhr721@gmail.com>
2024-06-24 15:20:55 +0900

    feature : 개별 템플릿 조회,삭제 요청 추가
    #36

--- src/components/consensus/myPage/Popup.jsx
+++ src/components/consensus/myPage/Popup.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import editIcon from '~/assets/icons/edit.svg';
 import deleteIcon from '~/assets/icons/delete.svg';
 
-export default function Popup({ onDelete, valuationId }) {
+export default function Popup({ onDelete, onEdit }) {
   return (
     <div className="absolute bottom-full mb-2 right-2 text-tuatara-50 w-32 bg-tuatara-700 rounded-lg shadow-lg p-2">
       <div className="flex flex-col space-y-2">
-        <div className="flex flex-row justify-between items-center p-2 hover:bg-tuatara-800">
+        <div
+          className="flex flex-row justify-between items-center p-2 hover:bg-tuatara-800"
+          onClick={() => {
+            onEdit();
+          }}
+          // onClick={() =>
+          //   navigate(`${URI_PATH.valuationEditPage}/?id=${valuationId}`)
+          // }
+        >
           <button className="text-left rounded">수정</button>
           <img src={editIcon} className="w-6 h-6" />
         </div>
@@ -17,9 +25,7 @@ export default function Popup({ onDelete, valuationId }) {
             type="button"
             onClick={(e) => {
               e.stopPropagation();
-
-              console.log(e);
-              onDelete(valuationId);
+              onDelete();
             }}
           >
             삭제
--- src/components/consensus/myPage/Template.jsx
+++ src/components/consensus/myPage/Template.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react';
 import moreIcon from '~/assets/icons/more.svg';
 import Popup from '~/components/consensus/myPage/Popup';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { URI_PATH } from '../../../routers/main-router';
 
 export default function Template() {
   const [tempList, setTempList] = useState(null);
   const [showPopup, setShowPopup] = useState(null); // null로 초기화하여 팝업이 표시되지 않도록 설정
   const popupRef = useRef(null);
+  const navigate = useNavigate();
 
-  // moreIcon을 누르면, Popup이 뜨도록
-  // 만일  그 바깥 부분을 클릭하면, Popup이 닫히도록
   const togglePopup = (index) => {
     setShowPopup(showPopup === index ? null : index);
   };
@@ -19,43 +21,79 @@ export default function Template() {
     }
   };
 
+  const handleDelete = async (template_id) => {
+    try {
+      const token =
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozLCJpYXQiOjE3MTg5MzI5OTAsImV4cCI6MTcxOTc5Njk5MH0.BAl-EkK7ExHe2GiDpWb1sYWqu4rM-OzBJLZt23xecFA';
+      await axios.delete(`/api/template/${template_id}`, {
+        headers: {
+          auth: token,
+        },
+        data: { template_id },
+      });
+
+      setTempList(tempList.filter((item) => item.template_id !== template_id));
+    } catch (error) {
+      console.error('삭제 중 에러:', error);
+    }
+  };
+
   useEffect(() => {
-    setTempList(tmpTempList);
+    const fetchData = async () => {
+      try {
+        const token =
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozLCJpYXQiOjE3MTg5MzI5OTAsImV4cCI6MTcxOTc5Njk5MH0.BAl-EkK7ExHe2GiDpWb1sYWqu4rM-OzBJLZt23xecFA';
+        const response = await axios.get('/api/template', {
+          headers: {
+            auth: token,
+          },
+        });
+        console.log(response.data);
+        const transformedData = response.data.map((item) => ({
+          template_id: item.template_id,
+          template_name: item.template_name,
+          createdAt: new Date(item.createdAt).toLocaleDateString().slice(0, 11),
+        }));
+
+        setTempList(transformedData);
+      } catch (error) {
+        console.error('사용자의 템플릿을 가져오는데 실패하였습니다.', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
 
-  /* axios 이전, css를 위한 dump data */
-  const tmpTempList = [
-    {
-      tempName: 'DCF 밸류에이션',
-      date: '2024.06.10',
-    },
-    {
-      tempName: 'PER 밸류에이션',
-      date: '2024.06.10',
-    },
-    {
-      tempName: 'EV/EBITDA 밸류에이션',
-      date: '2024.06.10',
-    },
-  ];
-
   return (
     <div>
       <div className="mt-8">
         {tempList?.map((element, index) => {
           return (
-            <div className="flex justify-between text-body2 flex-row text-center items-center rounded-lg h-16 m-2 bg-tuatara-900">
+            <div
+              key={index}
+              className="flex justify-between text-body2 flex-row text-center items-center rounded-lg h-16 m-2 bg-tuatara-900"
+            >
               <div className="flex flex-row basis-3/5">
-                <div className="basis-1/3">{element.tempName}</div>
+                <div className="basis-1/3">{element.template_name}</div>
               </div>
               <div className="flex justify-end flex-row basis-2/5 mr-8">
-                <div className="basis-1/5 mr-2">{element.date}</div>
-                <div className="relative basis-1/7 mr-2 " ref={popupRef}>
-                  {showPopup === index && <Popup />}
+                <div className="basis-1/5 mr-2">{element.createdAt}</div>
+                <div className="relative basis-1/7 mr-2 ">
+                  {showPopup === index && (
+                    <div ref={popupRef}>
+                      <Popup
+                        onDelete={() => handleDelete(element.template_id)}
+                        onEdit={() => navigate(`$(URI_PATH.templateEditPage)/>id=${element.template_id}`)}
+                      />
+                    </div>
+                  )}
                   <img
                     src={moreIcon}
                     alt="moreIcon"
