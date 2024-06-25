73b520600a68365743569631d8a048f7b83d1d9a
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-06-26 08:05:27 +0900

    style : 코드 컨벤션 수정

--- src/components/consensus/home/StockList.jsx
+++ src/components/consensus/home/StockList.jsx
@@ -15,6 +15,7 @@ export default function StockList({ data, type }) {
       setArrowIcon(downArrow);
     }
   }, [type]);
+
   return (
     <div>
       <div className="mt-8">
@@ -27,22 +28,25 @@ export default function StockList({ data, type }) {
         </div>
         {data?.map((element, index) => {
           return (
-            <Link to={`${URI_PATH.consensusPage}/${index}`}>
+            <Link
+              to={`${URI_PATH.consensusPage}/${element.stock_code}`}
+              state={element.company_name}
+            >
               <div className="flex justify-between text-tuatara-50 text-caption flex-row text-center items-center rounded-lg h-16 m-2 bg-tuatara-950">
                 <div className="flex flex-row w-full">
-                  <div className="basis-1/5">{element.stockName}</div>
+                  <div className="basis-1/5">{element.company_name}</div>
                   <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
                     <img src={arrowIcon} className="w-3 h-3" />
-                    <div>{element.pastValuePotential * 100}%</div>
+                    <div>{element.target_price * 100}%</div>
                   </div>
                   <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
                     <img src={arrowIcon} className="w-3 h-3" />
                     <div>{element.currentValuePotential * 100}%</div>
                   </div>
-                  <div className="basis-1/5">{element.currentPrice}원</div>
+                  <div className="basis-1/5">{element.target_price}원</div>
                   <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
                     <img src={arrowIcon} className="w-3 h-3" />
-                    <div>{element.targetPrice}원</div>
+                    <div>{element.target_price}원</div>
                   </div>
                 </div>
               </div>
