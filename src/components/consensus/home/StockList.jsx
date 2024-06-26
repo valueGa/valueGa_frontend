992a0202008cbaad859280c618972fc7fe2a2592
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-06-26 09:35:17 +0900

    refactor : 아이콘 변경

--- src/components/consensus/home/StockList.jsx
+++ src/components/consensus/home/StockList.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { URI_PATH } from '~/routers/main-router';
-import './ArrowStyle.css'; // 기존 CSS 파일 import
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { URI_PATH } from "~/routers/main-router";
+import "./ArrowStyle.css"; // 기존 CSS 파일 import
 
 export default function StockList({ data, type }) {
-  const [arrowClass, setArrowClass] = useState('');
+  const [arrowClass, setArrowClass] = useState("");
 
   useEffect(() => {
-    if (type === 'buy') {
-      setArrowClass('arrow-icon up');
+    if (type === "buy") {
+      setArrowClass("arrow-icon up");
     } else {
-      setArrowClass('arrow-icon down');
+      setArrowClass("arrow-icon down");
     }
   }, [type]);
 
@@ -26,18 +26,15 @@ export default function StockList({ data, type }) {
         </div>
         {data?.map((element, index) => {
           return (
-
             <Link
               to={`${URI_PATH.consensusPage}/${element.stock_code}`}
               state={element.company_name}
             >
-
               <div className="flex justify-between text-tuatara-50 text-caption flex-row text-center items-center rounded-lg h-16 m-2 bg-tuatara-950">
                 <div className="flex flex-row w-full">
                   <div className="basis-1/5">{element.company_name}</div>
                   <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
-
-                    <img src={arrowIcon} className="w-3 h-3" />
+                    <img src={arrowClass} className="w-3 h-3" />
                     <div>{element.target_price * 100}%</div>
                   </div>
                   <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
@@ -46,10 +43,8 @@ export default function StockList({ data, type }) {
                   </div>
                   <div className="basis-1/5">{element.target_price}원</div>
                   <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
-
-                    <img src={arrowIcon} className="w-3 h-3" />
+                    <img src={arrowClass} className="w-3 h-3" />
                     <div>{element.target_price}원</div>
-
                   </div>
                 </div>
               </div>
