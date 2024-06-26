751c6b3975566f9d6c3a05a45f5f9a1d2100da24
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-06-26 20:23:40 +0900

    refactor : CSS수정

--- src/components/consensus/home/StockList.jsx
+++ src/components/consensus/home/StockList.jsx
@@ -5,7 +5,6 @@ import "./ArrowStyle.css"; // 기존 CSS 파일 import
 
 export default function StockList({ data, type }) {
   const [arrowClass, setArrowClass] = useState("");
-
   useEffect(() => {
     if (type === "buy") {
       setArrowClass("arrow-icon up");
@@ -17,32 +16,41 @@ export default function StockList({ data, type }) {
   return (
     <div>
       <div className="mt-8">
-        <div className="flex flex-row text-body2 text-tuatara-200 text-center items-center h-7 mt-6">
-          <div className="basis-1/5 ">종목명</div>
-          <div className="basis-1/5">과거 상승 여력</div>
-          <div className="basis-1/5">현재 상승 여력</div>
-          <div className="basis-1/5">현재 주가</div>
-          <div className="basis-1/5">목표 주가</div>
+        <div className="flex flex-row text-body2 text-tuatara-200 text-center items-center justify-between h-7 mt-6">
+          <div className="flex-1 text-center">종목명</div>
+          <div className="flex-1 text-center">현재 상승 여력</div>
+          <div className="flex-1 text-center">현재 주가</div>
+          <div className="flex-1 text-center">목표 주가</div>
         </div>
+
         {data?.map((element, index) => {
           return (
             <Link
               to={`${URI_PATH.consensusPage}/${element.stock_code}`}
-              state={element.company_name}
+              state={{
+                companyName: element.company_name,
+                type: type,
+              }}
             >
-              <div className="flex justify-between text-tuatara-50 text-caption flex-row text-center items-center rounded-lg h-16 m-2 bg-tuatara-950">
-                <div className="flex flex-row w-full">
-                  <div className="basis-1/5">{element.company_name}</div>
-                  <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
-                    <img src={arrowClass} className="w-3 h-3" />
-                    <div>{element.target_price * 100}%</div>
+              <div className="flex justify-between items-center text-tuatara-50 text-caption flex-row text-center rounded-lg h-16 m-2 bg-tuatara-950">
+                <div className="flex flex-row w-full items-center justify-between">
+                  <div className="flex-1 text-center">
+                    {element.company_name}
                   </div>
-                  <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
+
+                  <div className="flex-1 flex flex-row space-x-2 items-center justify-center">
                     <div className={arrowClass} />
-                    <div>{element.currentValuePotential * 100}%</div>
+                    <div>
+                      {(element.target_price / element.currentPrice).toFixed(2)}
+                      %
+                    </div>
                   </div>
-                  <div className="basis-1/5">{element.target_price}원</div>
-                  <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
+
+                  <div className="flex-1 text-center">
+                    {element.currentPrice}원
+                  </div>
+
+                  <div className="flex-1 flex flex-row space-x-2 items-center justify-center">
                     <img src={arrowClass} className="w-3 h-3" />
                     <div>{element.target_price}원</div>
                   </div>
