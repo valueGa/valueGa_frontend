dc0f94475a0da112e6d266510060ccd8b26530d8
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-06-26 10:46:42 +0900

    refactor : 변수이름 수정

--- src/components/consensus/home/ConsensusHome.jsx
+++ src/components/consensus/home/ConsensusHome.jsx
@@ -11,8 +11,8 @@ export default function ConsensusHome() {
   const navigate = useNavigate();
   const [top5List, setTop5List] = useState([]);
   const [down5List, setDown5List] = useState([]);
-  const [allTopList, setAllTopList] = useState([]);
-  const [allDownList, setAllDownList] = useState([]);
+  const [allBuyList, setAllBuyList] = useState([]);
+  const [allSellList, setAllSellList] = useState([]);
 
   const [allBuyCount, setAllBuyCount] = useState(0);
   const [allSellCount, setAllSellCount] = useState(0);
@@ -22,40 +22,7 @@ export default function ConsensusHome() {
       try {
         const result = await getConsensus();
 
-        const initialTop5List = [
-          {
-            company_name: result.data.overvaluedList.top1.company_name,
-            stock_code: result.data.overvaluedList.top1.stock_code,
-            value_potential: result.data.overvaluedList.top1.value_potential,
-            target_price: result.data.overvaluedList.top1.target_price,
-          },
-          {
-            company_name: result.data.overvaluedList.top2.company_name,
-            stock_code: result.data.overvaluedList.top2.stock_code,
-            value_potential: result.data.overvaluedList.top2.value_potential,
-            target_price: result.data.overvaluedList.top2.target_price,
-          },
-          {
-            company_name: result.data.overvaluedList.top3.company_name,
-            stock_code: result.data.overvaluedList.top3.stock_code,
-            value_potential: result.data.overvaluedList.top3.value_potential,
-            target_price: result.data.overvaluedList.top3.target_price,
-          },
-          {
-            company_name: result.data.overvaluedList.top4.company_name,
-            stock_code: result.data.overvaluedList.top4.stock_code,
-            value_potential: result.data.overvaluedList.top4.value_potential,
-            target_price: result.data.overvaluedList.top4.target_price,
-          },
-          {
-            company_name: result.data.overvaluedList.top5.company_name,
-            stock_code: result.data.overvaluedList.top5.stock_code,
-            value_potential: result.data.overvaluedList.top5.value_potential,
-            target_price: result.data.overvaluedList.top5.target_price,
-          },
-        ];
-
-        const initialDown5List = [
+        const initialBuy5List = [
           {
             company_name: result.data.undervaluedList.top1.company_name,
             stock_code: result.data.undervaluedList.top1.stock_code,
@@ -87,11 +54,44 @@ export default function ConsensusHome() {
             target_price: result.data.undervaluedList.top5.target_price,
           },
         ];
-        setTop5List(initialTop5List);
-        setDown5List(initialDown5List);
 
-        setAllDownList(initialDown5List);
-        setAllTopList(initialTop5List);
+        const initialSell5List = [
+          {
+            company_name: result.data.overvaluedList.top1.company_name,
+            stock_code: result.data.overvaluedList.top1.stock_code,
+            value_potential: result.data.overvaluedList.top1.value_potential,
+            target_price: result.data.overvaluedList.top1.target_price,
+          },
+          {
+            company_name: result.data.overvaluedList.top2.company_name,
+            stock_code: result.data.overvaluedList.top2.stock_code,
+            value_potential: result.data.overvaluedList.top2.value_potential,
+            target_price: result.data.overvaluedList.top2.target_price,
+          },
+          {
+            company_name: result.data.overvaluedList.top3.company_name,
+            stock_code: result.data.overvaluedList.top3.stock_code,
+            value_potential: result.data.overvaluedList.top3.value_potential,
+            target_price: result.data.overvaluedList.top3.target_price,
+          },
+          {
+            company_name: result.data.overvaluedList.top4.company_name,
+            stock_code: result.data.overvaluedList.top4.stock_code,
+            value_potential: result.data.overvaluedList.top4.value_potential,
+            target_price: result.data.overvaluedList.top4.target_price,
+          },
+          {
+            company_name: result.data.overvaluedList.top5.company_name,
+            stock_code: result.data.overvaluedList.top5.stock_code,
+            value_potential: result.data.overvaluedList.top5.value_potential,
+            target_price: result.data.overvaluedList.top5.target_price,
+          },
+        ];
+        setTop5List(initialBuy5List);
+        setDown5List(initialSell5List);
+
+        setAllSellList(initialSell5List);
+        setAllBuyList(initialBuy5List);
         setAllBuyCount(result.data.totalList.buyCount);
         setAllSellCount(result.data.totalList.sellCount);
       } catch (error) {
@@ -107,57 +107,6 @@ export default function ConsensusHome() {
     const result = await getConsensusMore(index);
 
     if (activeIndex === 0) {
-      console.log(`${index}, ${allSellCount}`);
-      if (index >= allSellCount) {
-      } else {
-        const moreSellList = [];
-        if (result.data.overvaluedList.top1 != null) {
-          moreSellList.push({
-            company_name: result.data.overvaluedList.top1.company_name,
-            stock_code: result.data.overvaluedList.top1.stock_code,
-            value_potential: result.data.overvaluedList.top1.value_potential,
-            target_price: result.data.overvaluedList.top1.target_price,
-          });
-        }
-        if (result.data.overvaluedList.top2 != null) {
-          moreSellList.push({
-            company_name: result.data.overvaluedList.top2.company_name,
-            stock_code: result.data.overvaluedList.top2.stock_code,
-            value_potential: result.data.overvaluedList.top2.value_potential,
-            target_price: result.data.overvaluedList.top2.target_price,
-          });
-        }
-        if (result.data.overvaluedList.top3 != null) {
-          moreSellList.push({
-            company_name: result.data.overvaluedList.top3.company_name,
-            stock_code: result.data.overvaluedList.top3.stock_code,
-            value_potential: result.data.overvaluedList.top3.value_potential,
-            target_price: result.data.overvaluedList.top3.target_price,
-          });
-        }
-        if (result.data.overvaluedList.top4 != null) {
-          moreSellList.push({
-            company_name: result.data.overvaluedList.top4.company_name,
-            stock_code: result.data.overvaluedList.top4.stock_code,
-            value_potential: result.data.overvaluedList.top4.value_potential,
-            target_price: result.data.overvaluedList.top4.target_price,
-          });
-        }
-        if (result.data.overvaluedList.top5 != null) {
-          moreSellList.push({
-            company_name: result.data.overvaluedList.top5.company_name,
-            stock_code: result.data.overvaluedList.top5.stock_code,
-            value_potential: result.data.overvaluedList.top5.value_potential,
-            target_price: result.data.overvaluedList.top5.target_price,
-          });
-        }
-
-        setAllTopList((prevList) => [...prevList, ...moreSellList]);
-        console.log(
-          `${moreSellList[0].company_name}, ${moreSellList[1].company_name}, ${moreSellList[2].company_name}`
-        );
-      }
-    } else {
       console.log(`${index}, ${allBuyCount}`);
       if (index >= allBuyCount) {
       } else {
@@ -203,9 +152,60 @@ export default function ConsensusHome() {
           });
         }
 
-        setAllDownList((prevList) => [...prevList, ...moreBuyList]);
+        setAllBuyList((prevList) => [...prevList, ...moreBuyList]);
         console.log(
           `${moreBuyList[0].company_name}, ${moreBuyList[1].company_name}, ${moreBuyList[2].company_name}`
+        );
+      }
+    } else {
+      console.log(`${index}, ${allSellCount}`);
+      if (index >= allSellCount) {
+      } else {
+        const moreSellList = [];
+        if (result.data.overvaluedList.top1 != null) {
+          moreSellList.push({
+            company_name: result.data.overvaluedList.top1.company_name,
+            stock_code: result.data.overvaluedList.top1.stock_code,
+            value_potential: result.data.overvaluedList.top1.value_potential,
+            target_price: result.data.overvaluedList.top1.target_price,
+          });
+        }
+        if (result.data.overvaluedList.top2 != null) {
+          moreSellList.push({
+            company_name: result.data.overvaluedList.top2.company_name,
+            stock_code: result.data.overvaluedList.top2.stock_code,
+            value_potential: result.data.overvaluedList.top2.value_potential,
+            target_price: result.data.overvaluedList.top2.target_price,
+          });
+        }
+        if (result.data.overvaluedList.top3 != null) {
+          moreSellList.push({
+            company_name: result.data.overvaluedList.top3.company_name,
+            stock_code: result.data.overvaluedList.top3.stock_code,
+            value_potential: result.data.overvaluedList.top3.value_potential,
+            target_price: result.data.overvaluedList.top3.target_price,
+          });
+        }
+        if (result.data.overvaluedList.top4 != null) {
+          moreSellList.push({
+            company_name: result.data.overvaluedList.top4.company_name,
+            stock_code: result.data.overvaluedList.top4.stock_code,
+            value_potential: result.data.overvaluedList.top4.value_potential,
+            target_price: result.data.overvaluedList.top4.target_price,
+          });
+        }
+        if (result.data.overvaluedList.top5 != null) {
+          moreSellList.push({
+            company_name: result.data.overvaluedList.top5.company_name,
+            stock_code: result.data.overvaluedList.top5.stock_code,
+            value_potential: result.data.overvaluedList.top5.value_potential,
+            target_price: result.data.overvaluedList.top5.target_price,
+          });
+        }
+
+        setAllSellList((prevList) => [...prevList, ...moreSellList]);
+        console.log(
+          `${moreSellList[0].company_name}, ${moreSellList[1].company_name}, ${moreSellList[2].company_name}`
         );
       }
     }
@@ -219,10 +219,10 @@ export default function ConsensusHome() {
       <ConsensusList
         allBuyCount={allBuyCount}
         allSellCount={allSellCount}
-        allTopList={allTopList}
-        allDownList={allDownList}
-        setAllTopList={setAllTopList}
-        setAllDownList={setAllDownList}
+        allTopList={allBuyList}
+        allDownList={allSellList}
+        setAllTopList={setAllBuyList}
+        setAllDownList={setAllSellList}
         fetchMoreData={fetchMoreData}
       />
     </div>
