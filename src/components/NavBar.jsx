3db9cd6af5174bda296fe9cb11340bb121bfb4fd
KimRiun <kimriun27@gmail.com>
2024-06-26 20:29:30 +0900

    fix : 네비바 여백 수정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { URI_PATH } from "~/routers/main-router";
-import logo from "~/assets/icons/logo.svg";
-import search from "~/assets/icons/search.svg";
-import { useNavigate } from "react-router-dom";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { URI_PATH } from '~/routers/main-router';
+import logo from '~/assets/icons/logo.svg';
+import search from '~/assets/icons/search.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-row w-full text-caption text-tuatara-50 font-apple items-center p-5 px-14 justify-between">
+    <div className="flex flex-row w-full text-caption text-tuatara-50 font-apple items-center py-4 px-20 justify-between">
       <ul className="flex flex-row text-center items-center space-x-7">
         <li>
           <Link to="/">
@@ -28,7 +27,6 @@ export default function NavBar() {
       </ul>
       <ul className="flex flex-row text-center items-center space-x-7">
         <li>
-
           <Link to="#">
             <img src={search} alt="logo" className="w-8 h-8" />
           </Link>
@@ -36,13 +34,12 @@ export default function NavBar() {
         <li>
           <button
             onClick={() => {
-              localStorage.removeItem("valueGa_AccessToken");
+              localStorage.removeItem('valueGa_AccessToken');
               navigate(`${URI_PATH.mainPage}`);
             }}
           >
             Logout
           </button>
-
         </li>
       </ul>
     </div>
