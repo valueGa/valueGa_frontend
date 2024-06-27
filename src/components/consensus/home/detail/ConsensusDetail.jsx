a7626336ed1f83c2e4a4c7c77be7c3fa5e80af96
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-06-27 20:15:02 +0900

    refactor : 총유통주식에 콤마 적용

--- src/components/consensus/home/detail/ConsensusDetail.jsx
+++ src/components/consensus/home/detail/ConsensusDetail.jsx
@@ -1,26 +1,26 @@
-import { useParams, useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import ChartRatio from './ChartConsensus';
-import ChartStock from './ChartStock';
-import StockPredictionList from './StockPredictionList';
-import { getConsensusDetail } from '/src/apis/consensus';
+import { useParams, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import ChartRatio from "./ChartConsensus";
+import ChartStock from "./ChartStock";
+import StockPredictionList from "./StockPredictionList";
+import { getConsensusDetail } from "/src/apis/consensus";
 
 export default function ConsensusDetail() {
   const [financialInfo, setFinancialInfo] = useState([
     {
-      매출액: 'N/A',
-      영업이익: 'N/A',
-      총유통주식: 'N/A',
-      유보비율: 'N/A',
+      매출액: "N/A",
+      영업이익: "N/A",
+      총유통주식: "N/A",
+      유보비율: "N/A",
     },
     {
-      부채비율: 'N/A',
-      evebitda: 'N/A',
-      자기자본이익률: 'N/A',
-      주당순이익: 'N/A',
+      부채비율: "N/A",
+      evebitda: "N/A",
+      자기자본이익률: "N/A",
+      주당순이익: "N/A",
     },
     {
-      년도: '',
+      년도: "",
     },
   ]);
 
@@ -117,8 +117,8 @@ export default function ConsensusDetail() {
   const params = useParams();
   const location = useLocation();
   const [valuationList, setValuationList] = useState([]);
-  const [currentPrice, setCurrentPrice] = useState('');
-  const [targetPrice, setTargetPrice] = useState('');
+  const [currentPrice, setCurrentPrice] = useState("");
+  const [targetPrice, setTargetPrice] = useState("");
 
   const [closePriceList, setClosePriceList] = useState([]);
   const [ratioList, setRatioList] = useState([]);
@@ -128,24 +128,28 @@ export default function ConsensusDetail() {
       const result = await getConsensusDetail(`${params.id}`);
 
       setTargetPrice(result.data.consensusInfo.target_price);
-      setClosePriceList(result.data.chartInfo.map((element, index) => parseInt(element.closePrice, 10)));
+      setClosePriceList(
+        result.data.chartInfo.map((element, index) =>
+          parseInt(element.closePrice, 10)
+        )
+      );
       setRatioList(result.data.ratio);
       console.log(`s ${result.data.valuationList}`);
       setValuationList(result.data.valuationList);
       // 매출액을 업데이트하는 함수
-      updateSr(Number(result.data.financeInfos.sr).toLocaleString() + ' 원');
-      updateOi(Number(result.data.financeInfos.oi).toLocaleString() + ' 원');
+      updateSr(Number(result.data.financeInfos.sr).toLocaleString() + " 원");
+      updateOi(Number(result.data.financeInfos.oi).toLocaleString() + " 원");
       updateYear(
         result.data.financeInfos.year.toString().slice(0, 4) +
-          '-' +
+          "-" +
           result.data.financeInfos.year.toString().slice(4, 6)
       );
-      updateRr(result.data.financeInfos.rr + ' %');
-      updateDr(result.data.financeInfos.dr + ' %');
-      updateEvebitda(result.data.financeInfos.evebitda + ' %');
-      updateRoe(result.data.financeInfos.roe + ' %');
-      updateBps(Number(result.data.financeInfos.bps).toLocaleString() + ' 원');
-      updateTs(result.data.financeInfos.ts + ' 주');
+      updateRr(result.data.financeInfos.rr + " %");
+      updateDr(result.data.financeInfos.dr + " %");
+      updateEvebitda(result.data.financeInfos.evebitda + " %");
+      updateRoe(result.data.financeInfos.roe + " %");
+      updateBps(Number(result.data.financeInfos.bps).toLocaleString() + " 원");
+      updateTs(Number(result.data.financeInfos.ts).toLocaleString() + " 주");
       setCurrentPrice(result.data.currentPrice);
     };
 
@@ -154,38 +158,38 @@ export default function ConsensusDetail() {
 
   const dumys = [
     {
-      stockName: 'LG전자',
+      stockName: "LG전자",
       pastValuePotential: 0.5,
       currentValuePotential: 0.4,
-      writeDate: '2024-05-24',
+      writeDate: "2024-05-24",
       targetPrice: 120000,
     },
     {
-      stockName: '삼성전자',
+      stockName: "삼성전자",
       pastValuePotential: 0.4,
       currentValuePotential: 0.35,
-      writeDate: '2024-05-21',
+      writeDate: "2024-05-21",
       targetPrice: 85000,
     },
     {
-      stockName: '알테오젠',
+      stockName: "알테오젠",
       pastValuePotential: 0.3,
       currentValuePotential: 0.22,
-      writeDate: '2024-05-03',
+      writeDate: "2024-05-03",
       targetPrice: 278500,
     },
     {
-      stockName: '다우존스',
+      stockName: "다우존스",
       pastValuePotential: 0.2,
       currentValuePotential: 0.21,
-      writeDate: '2024-03-24',
+      writeDate: "2024-03-24",
       targetPrice: 11000,
     },
     {
-      stockName: 'HLB',
+      stockName: "HLB",
       pastValuePotential: 0.1,
       currentValuePotential: 0.2,
-      writeDate: '2024-04-20',
+      writeDate: "2024-04-20",
       targetPrice: 65000,
     },
   ];
@@ -194,18 +198,24 @@ export default function ConsensusDetail() {
     <div className="px-[180px] mb-20">
       <div>
         <div className="flex justify-center items-center gap-2 mt-4">
-          <p className="text-subheading text-body1">{location.state.companyName.split(' ')[0]}</p>
+          <p className="text-subheading text-body1">
+            {location.state.companyName.split(" ")[0]}
+          </p>
 
           <button
             className={`px-3 rounded-md ${
-              location.state.type === 'buy'
-                ? 'bg-pink-100 px-3 rounded-md'
-                : location.state.type === 'sell'
-                ? 'bg-spray-350'
-                : 'bg-pink-100'
+              location.state.type === "buy"
+                ? "bg-pink-100 px-3 rounded-md"
+                : location.state.type === "sell"
+                ? "bg-spray-350"
+                : "bg-pink-100"
             }`}
           >
-            {location.state.type === 'buy' ? 'Buy' : location.state.type === 'sell' ? 'Sell' : '기본'}
+            {location.state.type === "buy"
+              ? "Buy"
+              : location.state.type === "sell"
+              ? "Sell"
+              : "기본"}
           </button>
         </div>
         <p className="text-center text-tuatara-200">{params.id}</p>
@@ -225,7 +235,9 @@ export default function ConsensusDetail() {
                 <p className="text-spray-300 text-body2 text-right">N/A</p> */}
               </div>
 
-              <p className="text-spray-350 text-heading3 font-bold">{Number(currentPrice).toLocaleString()}</p>
+              <p className="text-spray-350 text-heading3 font-bold">
+                {Number(currentPrice).toLocaleString()}
+              </p>
             </div>
           </div>
           <div className="flex justify-between items-end mt-4">
@@ -240,7 +252,9 @@ export default function ConsensusDetail() {
                 <p className="text-pink-50 text-body2 text-right">N/A</p> */}
               </div>
 
-              <p className="text-pink-100 text-heading3 font-bold">{Number(targetPrice).toLocaleString()}</p>
+              <p className="text-pink-100 text-heading3 font-bold">
+                {Number(targetPrice).toLocaleString()}
+              </p>
             </div>
           </div>
           <ChartRatio data={ratioList} />
@@ -253,7 +267,9 @@ export default function ConsensusDetail() {
                 Object.keys(financialInfo[0]).map((key) => (
                   <div key={key} className="flex justify-between mb-2">
                     <p className="text-tuatara-300 font-bold">{key}</p>
-                    <p className="text-tuatara-300 font-regular">{financialInfo[0][key]}</p>
+                    <p className="text-tuatara-300 font-regular">
+                      {financialInfo[0][key]}
+                    </p>
                   </div>
                 ))}
             </div>
@@ -264,12 +280,16 @@ export default function ConsensusDetail() {
                 Object.keys(financialInfo[1]).map((key) => (
                   <div key={key} className="flex justify-between mb-2">
                     <p className="text-tuatara-300 font-bold">{key}</p>
-                    <p className="text-tuatara-300 font-regular">{financialInfo[1][key]}</p>
+                    <p className="text-tuatara-300 font-regular">
+                      {financialInfo[1][key]}
+                    </p>
                   </div>
                 ))}
             </div>
           </div>
-          <p className="mt-4 text-mini text-center">[{financialInfo[2].년도} 기준]</p>
+          <p className="mt-4 text-mini text-center">
+            [{financialInfo[2].년도} 기준]
+          </p>
         </div>
       </div>
       <ChartStock data={closePriceList} />
