6fd0985655ddeee7b308981c159bf5b65a5cf0e7
Ho-Da-Dak <godqhr721@gmail.com>
2024-06-27 17:15:50 +0900

    refactor: 콘솔로그 삭제
    #90

--- src/components/consensus/home/detail/StockPredictionList.jsx
+++ src/components/consensus/home/detail/StockPredictionList.jsx
@@ -1,15 +1,10 @@
-import React, { useEffect, useState } from "react";
-import upArrow from "~/assets/icons/upArrow.svg";
-import downArrow from "~/assets/icons/downArrow.svg";
-import { Link } from "react-router-dom";
-import { URI_PATH } from "~/routers/main-router";
+import React, { useEffect, useState } from 'react';
+import upArrow from '~/assets/icons/upArrow.svg';
+import downArrow from '~/assets/icons/downArrow.svg';
+import { Link } from 'react-router-dom';
+import { URI_PATH } from '~/routers/main-router';
 
 export default function StockPredictionList({ data, currentPrice }) {
-  // if (data == null) {
-  //   console.log(`null`);
-  // } else {
-  //   console.log(`sd`);
-  // }
   return (
     <div>
       <div className="mt-8">
@@ -30,34 +25,21 @@ export default function StockPredictionList({ data, currentPrice }) {
                 <div className="flex flex-row w-full">
                   <div className="basis-1/5">{element.user_name}</div>
                   <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
-                    <div>
-                      {(element.user_target_price / element.past_price).toFixed(
-                        2
-                      )}
-                      %
-                    </div>
+                    <div>{(element.user_target_price / element.past_price).toFixed(2)}%</div>
                   </div>
                   <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
-                    <div>
-                      {(element.user_target_price / currentPrice).toFixed(2)}%
-                    </div>
+                    <div>{(element.user_target_price / currentPrice).toFixed(2)}%</div>
                   </div>
-                  <div className="basis-1/5">
-                    {element.valuation_date.slice(0, 10)}
-                  </div>
+                  <div className="basis-1/5">{element.valuation_date.slice(0, 10)}</div>
                   <div className="basis-1/5 flex flex-row space-x-6 items-center justify-center">
-                    <div>
-                      {Number(element.user_target_price).toLocaleString()} 원
-                    </div>
+                    <div>{Number(element.user_target_price).toLocaleString()} 원</div>
                   </div>
                 </div>
               </div>
             );
           })
         ) : (
-          <p className="text-tuatara-400 text-center mt-10">
-            아직 벨류에이션 내역이 없습니다.
-          </p>
+          <p className="text-tuatara-400 text-center mt-10">아직 벨류에이션 내역이 없습니다.</p>
         )}
       </div>
     </div>
